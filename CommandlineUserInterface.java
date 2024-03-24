cfddc12bd33ce923bfff75de8572d34b2c6cdfa1
StephenDelaCruz <133613262+StephenDelaCruz@users.noreply.github.com>
2024-03-24 20:50:12 +1300

    CommandlineUserInterface.java

--- CommandlineUserInterface.java
+++ CommandlineUserInterface.java
@@ -16,9 +16,6 @@ public class CommandlineUserInterface {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Scanner scan = new Scanner(System.in);
-        
         System.out.println("Welcome user!");
         System.out.println("What would you like to do today?");
         System.out.println("(1) View all questions");
@@ -26,7 +23,37 @@ public class CommandlineUserInterface {
         System.out.println("(3) Remove question");
         System.out.println("(4) Take a quiz");
         System.out.println("(5) Exit the program");
-        
+
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            if (scan.hasNextInt()) {
+                int input = scan.nextInt();
+
+                if(input == 5){
+                    System.out.println("See you next time!");
+                    break;
+                }
+                
+                switch (input) {
+                    case 1:
+                        System.out.println("questions...");
+                        break;
+                    case 2:
+                        System.out.println("add...");
+                        break;
+                    case 3:
+                        System.out.println("remove...");
+                        break;
+                    case 4:
+                        System.out.println("quiz...");
+                        break;
+                    default:
+                        System.out.println("Invalid input");
+                }
+            }
+        }
+        scan.close();
     }
-    
+
 }
